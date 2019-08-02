74c0eaeac5c5aee7186816542d14b1ba9d1c73ad
licesma <36255528+licesma@users.noreply.github.com>
2019-08-02 18:32:25 -0500

    Added the stack library

---
+++ Stack.h
@@ -0,0 +1,43 @@
+#pragma once
+template <class T>
+ class ListNode {
+public:T val;
+	   ListNode* next;
+public: ListNode(T val) {
+	this->val = val;
+	this->next = nullptr;
+}
+
+};
+
+
+template <class T>
+class Stack {
+public:ListNode<T>* head;
+public:Stack() {
+	head = nullptr;
+}
+	   void  push(T data) {
+		   ListNode<T>* neuf; neuf = new ListNode<T>(data);
+		   nuef->next = head;
+		   head = neuf;
+	   }
+	   T peek() {
+		   if (head!= nullptr)
+				return head->val;
+		   else 
+			   throw "Stack is empty, nothing to peek";
+	   }
+	   T pop() {
+		   if (head != null) {
+			   T res = head->val;
+			   head = head->next;
+		   }
+		   else  throw "Stack is empty, nothing to pop";
+		   
+	   }
+	   bool isEmpty() {
+		   return head == nullptr;
+	   }
+};
+
