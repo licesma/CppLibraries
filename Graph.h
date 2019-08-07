e9850b12f9ce8e00ba9213f143a2a39457438331
licesma <36255528+licesma@users.noreply.github.com>
2019-08-07 10:46:39 -0500

    Added Graph Librarie

---
+++ Graph.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vector>
+using namespace std;
+class GraphNode {
+public:
+	int val;
+	bool visited;
+	vector<GraphNode*>* adyacent;
+public:
+	GraphNode(int val) {
+		this->val = val;
+		visited = false;
+		adyacent = new vector<GraphNode*>();
+	}
+	void Add(GraphNode* nd) {
+		adyacent->push_back(nd);
+		nd->dirAdd(this);
+	}
+	void dirAdd(GraphNode* nd) {
+		adyacent->push_back(nd);
+	}
+	void printAdyacent() {
+		cout << "["; GraphNode* pr = nullptr;
+		for (GraphNode* gr : *adyacent) {
+			if (pr != nullptr) {
+				cout << pr->val<<", ";
+			}
+			pr = gr;
+		}
+		cout << pr->val << "]";
+	}
+};
+class Graph {
+public:
+	vector<GraphNode*>* nodes;
+public:
+	Graph() {
+		nodes = new vector<GraphNode*>();
+	}
+	void add(GraphNode* gr) {
+		nodes->push_back(gr);
+	}
+	
+};
